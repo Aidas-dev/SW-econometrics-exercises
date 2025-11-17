1bce39a712a2ff9fb07d3eede7aea2146d0009e7
AidasLaptop <149402833+Aidas-dev@users.noreply.github.com>
2025-11-17 19:01:16 +0200

    Started a new chapter, Chapter5 structure is copied from Chapter4.

---
+++ Chapters/Chapter5/R code/install_R_packages.R
@@ -0,0 +1,10 @@
+# Install packages for econometrics project, jupyter notebook, and R kernel in RStudio
+packages <- c("devtools", "repr", "IRdisplay", "pbdZMQ", "uuid", "IRkernel", "data.table", "ggplot2", "readODS")
+
+# Install all packages with dependencies
+install.packages(packages, dependencies = TRUE)
+
+# Set up IRkernel for Jupyter
+IRkernel::installspec(user = FALSE)
+
+cat("Installation complete! Restart Jupyter and select R as kernel.\n")
